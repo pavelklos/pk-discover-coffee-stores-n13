b1dd1870ebf591b4125d33f689feac0d79a03a1f
Pavel Klos <pavelkloscz@gmail.com>
2022-11-18 16:35:04 +0100

    Updated API 'hello'

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const now = new Date()
+  res.status(200).json({ name: 'John Doe', now: now.toLocaleString() })
 }
