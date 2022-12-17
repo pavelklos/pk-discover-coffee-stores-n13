49d522d725f00707d309bacd33039198e22a02c4
Pavel Klos <pavelkloscz@gmail.com>
2022-12-17 10:19:10 +0100

    _app.js Solution with Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>© {new Date().getFullYear()} Ankita</p>
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
