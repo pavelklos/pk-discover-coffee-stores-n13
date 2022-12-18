4354b770927f85c4c82ab8098a4c03b133d28bd2
Pavel Klos <pavelkloscz@gmail.com>
2022-12-18 11:32:33 +0100

    Banner Component + Mobile First Development & Banner Component Styling

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/banner'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   // console.log('styles', styles)
+
+  const handleOnBannerBtnClick = () => {
+    console.log('hi banner button')
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +19,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner
+          buttonText='View stores nearby'
+          handleOnClick={handleOnBannerBtnClick}
+        />
       </main>
     </div>
   )
