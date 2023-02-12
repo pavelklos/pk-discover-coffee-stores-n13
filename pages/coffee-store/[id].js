433116957d90812a3ca37064279e8eacaa185ab0
Pavel Klos <pavelkloscz@gmail.com>
2023-02-12 12:58:58 +0100

    Add route using Link component: Add link back to home

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,17 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const CoffeeStore = () => {
   const router = useRouter()
   console.log('router', router)
   return (
     <div>
+      <Link href='/' legacyBehavior scroll={true}>
+        <a>Back to home</a>
+      </Link>
+      <br />
+      <Link href='https://www.google.com/'>Google.com</Link>
+      <hr />
       Coffee Store Page <b>{router.query.id}</b>
     </div>
   )
