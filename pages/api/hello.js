952b132a4d14d7f9f7a7393ad7f429699a282e29
Pavel Klos <pavelkloscz@gmail.com>
2023-02-17 08:52:52 +0100

    Hello World API route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  console.log('[API-GET] : hello.js')
   const now = new Date()
+  // console.log({ req, res, now })
   res.status(200).json({ name: 'John Doe', now: now.toLocaleString() })
 }
