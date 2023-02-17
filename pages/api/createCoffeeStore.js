e5d15fd32cb0b3484e58e0e1003459c2780591e7
Pavel Klos <pavelkloscz@gmail.com>
2023-02-17 16:04:52 +0100

    Basic Coffee Store API / Serverless function

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,8 +1,19 @@
-// const Airtable = require('airtable')
-import Airtable from 'airtable'
-
+const Airtable = require('airtable')
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_KEY
 )
 
 const table = base('coffee-stores')
+
+console.log({ table })
+
+const createCoffeeStore = (req, res) => {
+  console.log({ req })
+  if (req.method === 'POST') {
+    res.json({ message: 'Hi there' })
+  } else {
+    res.json({ message: 'method is GET' })
+  }
+}
+
+export default createCoffeeStore
