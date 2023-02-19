f5329979ae6dc17f6359b9bd597197fe01e3717c
Pavel Klos <pavelkloscz@gmail.com>
2023-02-19 15:19:57 +0100

    Voting Feature Design

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -109,7 +109,13 @@ const CoffeeStore = (initialProps) => {
 
   const { name, address, neighbourhood, imgUrl } = coffeeStore
 
-  const handleUpvoteButton = () => {}
+  const [votingCount, setVotingCount] = useState(1)
+
+  const handleUpvoteButton = () => {
+    console.log('handle upvote')
+    let count = votingCount + 1
+    setVotingCount(count)
+  }
 
   // console.log({ coffeeStore })
 
@@ -155,7 +161,7 @@ const CoffeeStore = (initialProps) => {
           )}
           <div className={styles.iconWrapper}>
             <Image src='/static/icons/star.svg' width='24' height='24' />
-            <p className={styles.text}>1</p>
+            <p className={styles.text}>{votingCount}</p>
           </div>
 
           <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
